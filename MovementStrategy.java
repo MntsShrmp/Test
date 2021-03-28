ad79effa732ad6b51383c25c2ec5a88c79e771d4
AtaYurtsever <matays99@gmail.com>
2021-03-28 13:55:56 +0300

    Update MovementStrategy.java

--- MovementStrategy.java
+++ MovementStrategy.java
@@ -4,4 +4,5 @@
 //Should I extend this class in the future to alter how enemies render, or does that change make it lose the main idea?
 interface MovementStrategy {
     void move(Enemy e);
+    void play(String s);
 }
